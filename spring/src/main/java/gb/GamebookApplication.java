4a7d3d55a79619ed2de0a4f08dac856e5debde03
ssjb2 <unova4@gmail.com>
2019-03-22 21:36:00 +0100

    first commit

---
+++ spring/src/main/java/gb/GamebookApplication.java
@@ -0,0 +1,13 @@
+package gb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamebookApplication.class, args);
+	}
+
+}

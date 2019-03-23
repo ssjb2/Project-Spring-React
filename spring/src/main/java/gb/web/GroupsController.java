7d4edbb995c1a51c85ff371726b986e25d276514
ssjb2 <unova4@gmail.com>
2019-03-23 09:13:49 +0100

    group object validation

--- spring/src/main/java/gb/web/GroupsController.java
+++ spring/src/main/java/gb/web/GroupsController.java
@@ -4,11 +4,14 @@ import gb.domain.Groups;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import gb.services.GroupsService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/groups")
@@ -18,7 +21,10 @@ public class GroupsController {
     GroupsService groupsService;
 
     @PostMapping("")
-    public ResponseEntity<Groups> createNewGroup(@RequestBody Groups groups){
+    public ResponseEntity<?> createNewGroup(@Valid @RequestBody Groups groups, BindingResult result){
+        if(result.hasErrors()){
+            return new ResponseEntity<String>("Invalid Group Object", HttpStatus.BAD_REQUEST);
+        }
         Groups groups1 = groupsService.saveOrUpdateGroup(groups);
         return new ResponseEntity<Groups>(groups1,HttpStatus.CREATED);
     }

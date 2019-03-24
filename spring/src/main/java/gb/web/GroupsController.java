2bfa3a698f42730713ada11ae364078ebf1aeaf8
ssjb2 <unova4@gmail.com>
2019-03-24 10:50:22 +0100

    Map error

--- spring/src/main/java/gb/web/GroupsController.java
+++ spring/src/main/java/gb/web/GroupsController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import gb.services.GroupsService;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/groups")
@@ -23,8 +27,14 @@ public class GroupsController {
     @PostMapping("")
     public ResponseEntity<?> createNewGroup(@Valid @RequestBody Groups groups, BindingResult result){
         if(result.hasErrors()){
-            return new ResponseEntity<String>("Invalid Group Object", HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMap = new HashMap<>();
+            for(FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(),error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String,String>>(errorMap, HttpStatus.BAD_REQUEST);
         }
+
+
         Groups groups1 = groupsService.saveOrUpdateGroup(groups);
         return new ResponseEntity<Groups>(groups1,HttpStatus.CREATED);
     }

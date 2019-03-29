3f2a00ffe90d9532d6ddda9a2b925e41de3def61
ssjb2 <unova4@gmail.com>
2019-03-29 14:02:50 +0100

    group form

--- react-client/src/components/Group/AddGroup.js
+++ react-client/src/components/Group/AddGroup.js
@@ -1,10 +1,76 @@
 import React, { Component } from "react";
 
 class AddGroup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      groupName: "",
+      groupIdentifier: "",
+      description: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    const newGroup = {
+      groupName: this.state.groupName,
+      groupIdentifier: this.state.groupIdentifier,
+      description: this.state.description
+    };
+    console.log(newGroup);
+  }
   render() {
     return (
-      <div>
-        <h1>Add group form</h1>
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h5 className="display-4 text-center">Create Group form</h5>
+              <hr />
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg "
+                    placeholder="Group Name"
+                    name="groupName"
+                    value={this.state.groupName}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Unique Group ID"
+                    name="groupIdentifier"
+                    value={this.state.groupIdentifier}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <textarea
+                    className="form-control form-control-lg"
+                    placeholder="Group Description"
+                    name="description"
+                    value={this.state.description}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <input
+                  type="submit"
+                  className="btn btn-primary btn-block mt-4"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

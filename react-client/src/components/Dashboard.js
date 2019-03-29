53901a1ff400668b7f90bc2b06b3037a5744b6c6
ssjb2 <unova4@gmail.com>
2019-03-29 11:55:11 +0100

    nav

--- react-client/src/components/Dashboard.js
+++ react-client/src/components/Dashboard.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import GroupItem from "./Group/GroupItem";
 
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h1 className="alert alert-warning">lul</h1>
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Groups</h1>
+              <br />
+              <a href="GroupForm.html" className="btn btn-lg btn-info">
+                Create a Group
+              </a>
+              <br />
+              <hr />
+            </div>
+            <GroupItem />
+          </div>
+        </div>
       </div>
     );
   }

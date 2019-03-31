8fddd463d7f738b52ed0ba527d7778b09cdb4b00
ssjb2 <unova4@gmail.com>
2019-03-31 21:42:33 +0200

    redux

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -5,18 +5,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Layout/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddGroup from "./components/Group/AddGroup";
-
+import { Provider } from "react-redux";
+import store from "./store";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Route exact path="/Dashboard" component={Dashboard} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/Dashboard" component={Dashboard} />
 
-          <Route exact path="/addGroup" component={AddGroup} />
-        </div>
-      </Router>
+            <Route exact path="/addGroup" component={AddGroup} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }

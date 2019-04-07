ebb7f113677625dd69607ddfd92c67f030fc882e
ssjb2 <unova4@gmail.com>
2019-04-07 17:38:05 +0200

    hotfix

--- spring/src/main/java/gb/services/GroupPostService.java
+++ spring/src/main/java/gb/services/GroupPostService.java
@@ -1,4 +1,34 @@
 package gb.services;
 
+import gb.domain.Backlog;
+import gb.domain.GroupPost;
+import gb.repositories.BacklogRepository;
+import gb.repositories.GroupPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.*;
+
+@Service
 public class GroupPostService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+    @Autowired
+    private GroupPostRepository groupPostRepository;
+
+    public GroupPost addGroupPost(String groupIdentifier, GroupPost groupPost) {
+
+        Backlog backlog = backlogRepository.findByGroupIdentifier(groupIdentifier);
+        groupPost.setBacklog(backlog);
+        Integer BacklogSequence = backlog.getGPSequence();
+        BacklogSequence++;
+        backlog.setGPSequence(BacklogSequence);
+        groupPost.setGroupSequence(groupIdentifier + "-" + BacklogSequence);
+        groupPost.setGroupIdentifier(groupIdentifier);
+
+        if(groupPost.getStatus()==""|| groupPost.getStatus()==null){
+            groupPost.setStatus("ACTIVE");
+        }
+    return groupPostRepository.save(groupPost);}
 }
--- spring/src/main/java/gb/web/BacklogController.java
+++ spring/src/main/java/gb/web/BacklogController.java
@@ -1,4 +1,38 @@
 package gb.web;
 
+import gb.domain.Backlog;
+import gb.domain.GroupPost;
+import gb.repositories.BacklogRepository;
+import gb.repositories.GroupPostRepository;
+import gb.services.GroupPostService;
+import gb.services.MapValidationErrorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/backlog")
+@CrossOrigin
 public class BacklogController {
+
+    @Autowired
+    private GroupPostService groupPostService;
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addGroupPostToBacklog(@Valid @RequestBody GroupPost groupPost, BindingResult result, @PathVariable
+                                                    String backlog_id){
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
+
+        GroupPost groupPost1 = groupPostService.addGroupPost(backlog_id, groupPost);
+
+        return new ResponseEntity<GroupPost>(groupPost1, HttpStatus.CREATED);
+    }
 }
